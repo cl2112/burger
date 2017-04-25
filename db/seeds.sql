c765045543970444edea4cbc737c9f23603a5c81
cl2112 <cl2112cl2112@gmail.com>
2017-04-25 13:45:53 -0400

    initial commit. started working on the orm.js file in the config folder

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('The Mack', false, now())
+, ('Big Devil', false, now())
+, ('The Ghetto Burger', false, now())
+, ('The Primetime', false, now())
+, ('The Black Label Burger', true, now())	
