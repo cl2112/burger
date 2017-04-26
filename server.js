bbfe22559355df0edb8b4e7a4cf689bdf89033ac
cl2112 <cl2112cl2112@gmail.com>
2017-04-26 18:49:19 -0400

    started working on the burger.js file in the models folder.

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const express = require("express");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 //====================================================================
+
